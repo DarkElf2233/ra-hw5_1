60b485e013d969c5445e7468dd9366fbdf603698
DarkElf2233 <fd.melnikoff@gmail.com>
2024-07-29 12:14:54 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
+
+import { MyCard } from './components/MyCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <MyCard>
+        <Card.Img variant="top" src="https://dummyimage.com/300" alt='...' />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </MyCard>
+      <br />
+      <MyCard>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in 
+            to additional content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </MyCard>
     </div>
   );
 }
